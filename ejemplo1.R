ac56c807cf4747998fca0834a38bf1ee76144901
NuclearGeorge <38961339+NuclearGeorge@users.noreply.github.com>
2018-11-30 16:12:30 -0600

    Update and rename oro.R to ejemplo1.R

--- ejemplo1.R
+++ ejemplo1.R
@@ -6,9 +6,10 @@
 Lista = read.csv("https://raw.githubusercontent.com/NuclearGeorge/ORO/master/oro_datos/181126.csv", header = FALSE)
 
 plot(Lista, lwd=2, type = "l", col="red", xlab = "Hora", ylab = "USD/oz", main="NY-Gold", cex=0.5, cex.main=1.75, cex.lab=1.5, cex.axis=1.0)
-min(Lista$V2)
-max(Lista$V2)
-
-#plot(Lista, las=1, bty="l", col="red", pch=19, main="Title", cex=0.5, cex.main=2.5, cex.lab=1.5, cex.axis=1.0)
-#Lista$hr <- Lista$V1
-#Lista$costo <- Lista$V2
+cm = min(datos$V2)
+cM = max(datos$V2)
+cP = mean(datos$V2)
+tm = which(datos$V2==cm)
+tM = which(datos$V2==cM)
+datos$V1[tm]
+datos$V1[tM]
