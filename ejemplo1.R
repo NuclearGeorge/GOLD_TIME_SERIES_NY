f9fcc3ebda94b79a519984204474895e4477c2a8
NuclearGeorge <38961339+NuclearGeorge@users.noreply.github.com>
2019-11-23 13:07:35 -0600

    Delete ejemplo1.R

--- ejemplo1.R
+++
@@ -1,9 +0,0 @@
-#*********************************************************************
-# ANALISIS DE GRAFICAS DE SERIE DE TIEMPO DEL ORO    
-#
-# Por: Dr. Jorge Chávez
-#*********************************************************************
-Lista = read.csv("https://raw.githubusercontent.com/NuclearGeorge/ORO/master/oro_datos/181126.csv", header = FALSE)
-
-plot(Lista, lwd=2, type = "l", col="red", xlab = "Hora", ylab = "USD/oz", main="NY-Gold", cex=0.5, cex.main=1.75, cex.lab=1.5, cex.axis=1.0)
-
